2739b68250349231c03ed74bd8fa72d0c16893cb
Aleksandar <aca@software-advantage.com>
2018-12-21 11:51:21 +0100

    - updated menu button

--- asseets/js/main.js
+++ asseets/js/main.js
@@ -1,3 +1,7 @@
 (function(){
     console.log("Let's start");
+    $(document).on('click', '.js-btn-menu', function() {
+        $(this).toggleClass('active');
+        $(this).siblings('nav').toggleClass('active');
+    });
 })();
